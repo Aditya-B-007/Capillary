d85e5122c51195dc44374d439914e670de05778a
Aditya-B-Dhruva <131037390+Aditya-B-007@users.noreply.github.com>
2026-03-25 10:01:51 +0530

    Update discovery_broadcaster.py

--- controller/discovery_broadcaster.py
+++ controller/discovery_broadcaster.py
@@ -6,13 +6,13 @@ from common.config import settings
 
 BROADCAST_IP = "255.255.255.255"
 PORT = settings.discovery_port
-
 parsed_broker = urlparse(settings.broker_url)
+redis_host = parsed_broker.hostname or "redis"
 redis_port = parsed_broker.port or 6379
 
 MESSAGE = {
     "type": "CONTROLLER_ANNOUNCE",
-    "redis_host": "192.168.1.10",   # change dynamically if needed
+    "redis_host": redis_host,
     "redis_port": redis_port
 }
 
@@ -22,7 +22,7 @@ def broadcast():
 
     while True:
         sock.sendto(json.dumps(MESSAGE).encode(), (BROADCAST_IP, PORT))
-        print("Broadcasted controller presence")
+        print(f"Broadcasted controller presence (Broker: {redis_host}:{redis_port})")
         time.sleep(2)
 
 if __name__ == "__main__":
